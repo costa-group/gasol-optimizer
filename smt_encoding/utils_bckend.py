0471afaa50c8bf70edca942f32d97eaf1e69abdf
alexcere <48130030+alexcere@users.noreply.github.com>
2021-10-07 20:38:42 +0200

    Added execution for sequences in the SMS

--- smt_encoding/utils_bckend.py
+++ smt_encoding/utils_bckend.py
@@ -1,3 +1,4 @@
+import copy
 import re
 # Miscellanea methods
 
@@ -23,3 +24,30 @@ def generate_generic_push_instruction(string):
     if string.startswith('PUSH'):
         return 'PUSH'
     return string
+
+
+def compute_final_stack_from_solution(initial_stack, user_instr, instr_seq):
+    final_stack = copy.deepcopy(initial_stack)
+    for instr in instr_seq:
+        print(final_stack, instr)
+        if type(instr) == int:
+            final_stack.insert(0, instr)
+        elif instr.startswith("SWAP"):
+            index = int(instr.lstrip("SWAP"))
+            final_stack[0], final_stack[index] = final_stack[index], final_stack[0]
+        elif instr.startswith("DUP"):
+            index = int(instr.lstrip("DUP"))
+            final_stack.insert(0, final_stack[index - 1])
+        elif instr.startswith("POP"):
+            final_stack.pop(0)
+        else:
+            current_instr = list(filter(lambda x: x['id'] == instr, user_instr))[0]
+            for stack_elem in current_instr['inpt_sk']:
+                if stack_elem != final_stack[0]:
+                    raise ValueError("Error with instruction " + str(instr) + " applied to stack " + str(final_stack))
+                else:
+                    final_stack.pop(0)
+            if current_instr['outpt_sk']:
+                final_stack.insert(0, current_instr['outpt_sk'][0])
+    return final_stack
+
