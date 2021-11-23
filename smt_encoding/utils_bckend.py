ec3818189452a3465f679b6c7462681b8a08c2ff
alexcere <48130030+alexcere@users.noreply.github.com>
2021-11-23 13:17:50 +0100

    Change final stack comparison in gasol_encoder

--- smt_encoding/utils_bckend.py
+++ smt_encoding/utils_bckend.py
@@ -25,12 +25,18 @@ def generate_generic_push_instruction(string):
         return 'PUSH'
     return string
 
+def is_hex(s):
+    try:
+        int(s, 16)
+        return True
+    except ValueError:
+        return False
 
 def compute_final_stack_from_solution(initial_stack, user_instr, instr_seq):
     final_stack = copy.deepcopy(initial_stack)
     for instr in instr_seq:
-        if type(instr) == int:
-            final_stack.insert(0, instr)
+        if is_hex(instr):
+            final_stack.insert(0, int(instr, 16))
         elif instr.startswith("SWAP"):
             index = int(instr.lstrip("SWAP"))
             final_stack[0], final_stack[index] = final_stack[index], final_stack[0]
@@ -40,22 +46,34 @@ def compute_final_stack_from_solution(initial_stack, user_instr, instr_seq):
         elif instr.startswith("POP"):
             final_stack.pop(0)
         else:
-            current_instr = list(filter(lambda x: x['id'] == instr, user_instr))[0]
-            if current_instr['commutative']:
-                inpt_stack = current_instr['inpt_sk']
-                if (inpt_stack[0] != final_stack[0] or inpt_stack[1] != final_stack[1]) and\
-                        (inpt_stack[0] != final_stack[0] or inpt_stack[1] != final_stack[1]):
-                    raise ValueError("Error with instruction " + str(instr) + " applied to stack " + str(final_stack))
-                else:
-                    final_stack.pop(0)
-                    final_stack.pop(0)
-            else:
-                for stack_elem in current_instr['inpt_sk']:
-                    if stack_elem != final_stack[0]:
-                        raise ValueError("Error with instruction " + str(instr) + " applied to stack " + str(final_stack))
+            current_instrs = list(filter(lambda x: x['id'].startswith(instr), user_instr))
+            correct_instr = False
+            for current_instr in current_instrs:
+                if current_instr['commutative']:
+                    inpt_stack = current_instr['inpt_sk']
+                    if (inpt_stack[0] != final_stack[0] or inpt_stack[1] != final_stack[1]) and\
+                            (inpt_stack[0] != final_stack[0] or inpt_stack[1] != final_stack[1]):
+                        continue
                     else:
                         final_stack.pop(0)
-            if current_instr['outpt_sk']:
-                final_stack.insert(0, current_instr['outpt_sk'][0])
+                        final_stack.pop(0)
+                        correct_instr = True
+                else:
+                    all_right = True
+                    for i, stack_elem in enumerate(current_instr['inpt_sk']):
+                        if stack_elem != final_stack[i]:
+                            all_right = False
+                            break
+                    if all_right:
+                        for _ in current_instr['inpt_sk']:
+                            final_stack.pop(0)
+                        correct_instr = True
+                if not correct_instr:
+                    continue
+                if current_instr['outpt_sk']:
+                    final_stack.insert(0, current_instr['outpt_sk'][0])
+
+            if not correct_instr:
+                raise ValueError("No instruction matches " + str(current_instrs) + " with stack " + str(final_stack))
     return final_stack
 
