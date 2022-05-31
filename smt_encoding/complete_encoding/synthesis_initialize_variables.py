b9001965c5b9784a8df0882c0627c432c3da0419
alexcere <48130030+alexcere@users.noreply.github.com>
2022-05-31 18:51:15 +0200

    #9 Fit synthesis functions for stack encoding

--- smt_encoding/complete_encoding/synthesis_initialize_variables.py
+++ smt_encoding/complete_encoding/synthesis_initialize_variables.py
@@ -3,11 +3,12 @@ from typing import List, Union
 from smt_encoding.constraints.assertions import AssertHard
 from smt_encoding.constraints.connector_factory import add_and, add_eq, add_not
 from smt_encoding.complete_encoding.synthesis_functions import SynthesisFunctions
+from smt_encoding.instructions.encoding_instruction import Stack_Var_T
 
 # Methods for defining the state of the stack in the encoding at a given position
 
 
-def stack_encoding_for_position(j: int, sf: SynthesisFunctions, stack_state: List[Union[ExpressionReference, int, bool]],
+def stack_encoding_for_position(j: int, sf: SynthesisFunctions, stack_state: List[Stack_Var_T],
                                 bs: int) -> List[AssertHard]:
     """
     Methods for defining the state of the stack in the encoding at a given position
@@ -20,8 +21,10 @@ def stack_encoding_for_position(j: int, sf: SynthesisFunctions, stack_state: Lis
     """
 
     # Constraints for asserting the state of the stack
-    constraints = [AssertHard(add_and(sf.u(alpha, j), add_eq(sf.x(alpha, j), variable)))
-                   for alpha, variable in enumerate(stack_state)]
+    constraints = [AssertHard(add_and(sf.u(alpha, j), add_eq(sf.x(alpha, j), stack_var)))
+                   if type(stack_var) == int else
+                   AssertHard(add_and(sf.u(alpha, j), add_eq(sf.x(alpha, j), sf.stack_var(stack_var))))
+                   for alpha, stack_var in enumerate(stack_state)]
 
     # Constraints for asserting the remaining positions have no elements
     constraints.extend([AssertHard(add_not(sf.u(beta, j))) for beta in range(len(stack_state), bs)])
