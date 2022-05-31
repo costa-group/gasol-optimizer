f303b0b3d1c8912fffb7e7c3c1da4f959fd5c28b
alexcere <48130030+alexcere@users.noreply.github.com>
2022-05-31 14:43:26 +0200

    #9 Consider initial stack when initializing term conversion

--- smt_encoding/complete_encoding/synthesis_opcode_term_creation.py
+++ smt_encoding/complete_encoding/synthesis_opcode_term_creation.py
@@ -1,8 +1,9 @@
 
 from smt_encoding.instructions.uninterpreted_instruction import UninterpretedInstruction
-from smt_encoding.instructions.encoding_instruction import InstructionSubset
+from smt_encoding.instructions.encoding_instruction import InstructionSubset, Stack_Var_T
 from smt_encoding.constraints.function import ExpressionReference, Function, Sort
 from smt_encoding.constraints.connector import Formula_T
+from collections import Set
 from typing import List, Dict, Tuple, Union
 
 # TODO: instead of returning Dict[str, Formula_T], return Dict[str, List[Formula_T]] to allow stack vars being obtained
@@ -11,31 +12,43 @@ from typing import List, Dict, Tuple, Union
 
 class UninterpretedOpcodeTermCreation:
 
-    def __init__(self, instructions: List[UninterpretedInstruction], sort_type: Sort = Sort.integer):
+    def __init__(self, instructions: List[UninterpretedInstruction], initial_stack: List[Stack_Var_T],
+                 sort_type: Sort = Sort.integer):
         """
         :param instructions: instructions to generate an ExpressionReference
+        :param initial_stack: elements contained in the initial stack (ground elements)
         :param sort_type: sort range for the uninterpreted generated expressions
         """
         self._instructions = instructions
+
+        # We need only to consider those stack variables that are strings. Int stack variables are directly encoded
+        # using that representation
+        self._ground_stack_elements = initial_stack
         self._sort_type = sort_type
 
         self._stack_element_to_instr = {instruction.output_stack: instruction
-                                             for instruction in instructions if instruction.output_stack is not None}
+                                        for instruction in instructions if instruction.output_stack is not None}
 
     def opcode_rep_with_stack_vars(self) -> Tuple[Dict[str, Formula_T], List[Function]]:
         counter = 0
-        id_to_term = dict()
+        stack_var_to_term = dict()
         created_functions = []
+        
+        for stack_var in self._ground_stack_elements:
+            function = Function(f"s_{counter}", self._sort_type)
+            counter += 1
+            stack_var_to_term[stack_var] = function()
+            created_functions.append(function)
 
         for instruction in self._instructions:
             # We are considering only instructions that actually output a value
             if instruction.output_stack is not None:
                 function = Function(f"s_{counter}", self._sort_type)
                 counter += 1
-                id_to_term[instruction.output_stack] = function()
+                stack_var_to_term[instruction.output_stack] = function()
                 created_functions.append(function)
 
-        return id_to_term, created_functions
+        return stack_var_to_term, created_functions
 
     def opcode_rep_with_int(self, initial_int: int = 0) -> Tuple[Dict[str, Formula_T], List[Function]]:
         """
@@ -44,16 +57,22 @@ class UninterpretedOpcodeTermCreation:
         :return: a tuple with the conversion from instruction id to the corresponding integer and list representing
         the created functions (empty in this case)
         """
-        id_to_term = {instruction.output_stack: (i + initial_int) for i, instruction in
-                      enumerate((instr for instr in self._instructions if instr.output_stack is not None))}
-        return id_to_term, []
+        ground_stack_var_to_term = {stack_var: (i + initial_int)
+                                    for i, stack_var in enumerate(self._ground_stack_elements)}
+
+        next_int = initial_int + len(ground_stack_var_to_term)
+        
+        instr_stack_var_to_term = {instruction.output_stack: (i + next_int) for i, instruction in
+                                   enumerate((instr for instr in self._instructions if instr.output_stack is not None))}
+        
+        return dict(ground_stack_var_to_term, **instr_stack_var_to_term), []
 
     def _opcode_rep_with_uf(self, instruction: UninterpretedInstruction, sort_type: Sort,
-                            id_to_term: Dict[str, ExpressionReference],
+                            stack_var_to_term: Dict[str, ExpressionReference],
                             functors: Dict[str, Function]) -> Union[int, ExpressionReference]:
 
-        if instruction.output_stack in id_to_term:
-            return id_to_term[instruction.output_stack]
+        if instruction.output_stack in stack_var_to_term:
+            return stack_var_to_term[instruction.output_stack]
 
         arguments = []
 
@@ -62,26 +81,32 @@ class UninterpretedOpcodeTermCreation:
                 arguments.append(input_element)
             else:
                 arguments.append(self._opcode_rep_with_uf(self._stack_element_to_instr[input_element],
-                                                             sort_type, id_to_term, functors))
+                                                          sort_type, stack_var_to_term, functors))
 
         # Functor name: remove all whitespaces (such as "PUSH [$]" or "PUSH #[$]") and lower the opcode name
         functor_name = instruction.opcode_name.replace(' ', '').lower()
         functor = Function(functor_name,
-                           *[arg.type if type(arg) == ExpressionReference else Sort.integer for arg in arguments], sort_type)
+                           *[arg.type if type(arg) == ExpressionReference else Sort.integer for arg in arguments],
+                           sort_type)
         term_encoding = ExpressionReference(functor, *arguments)
 
-        id_to_term[instruction.output_stack] = term_encoding
+        stack_var_to_term[instruction.output_stack] = term_encoding
         functors[instruction.output_stack] = functor
 
         return term_encoding
 
     def opcode_rep_with_uf(self) -> Tuple[Dict[str, Formula_T], List[Function]]:
-        id_to_term: Dict[str, ExpressionReference] = dict()
+        stack_var_to_term: Dict[str, ExpressionReference] = dict()
         functors: Dict[str, Function] = dict()
+        
+        for i, stack_var in enumerate(self._ground_stack_elements):
+            function = Function(f"s_{i}", self._sort_type)
+            stack_var_to_term[stack_var] = function()
+            functors[stack_var] = function
 
         for instruction in self._instructions:
             # We are considering only non-store operations, as store operations do not have an uninterpreted
             # function so far
             if instruction.instruction_subset != InstructionSubset.store:
-                self._opcode_rep_with_uf(instruction, self._sort_type, id_to_term, functors)
-        return id_to_term, list(functors.values())
+                self._opcode_rep_with_uf(instruction, self._sort_type, stack_var_to_term, functors)
+        return stack_var_to_term, list(functors.values())
--- tests/test_uop_creation.py
+++ tests/test_uop_creation.py
@@ -7,7 +7,7 @@ from smt_encoding.constraints.function import Function, Sort
 class MyTestCase(unittest.TestCase):
 
     def test_empty_instructions_creation(self):
-        uop_creation = UninterpretedOpcodeTermCreation([])
+        uop_creation = UninterpretedOpcodeTermCreation([], [])
         self.assertTupleEqual(uop_creation.opcode_rep_with_int(), (dict(), []))
         self.assertTupleEqual(uop_creation.opcode_rep_with_uf(), (dict(), []))
         self.assertTupleEqual(uop_creation.opcode_rep_with_stack_vars(), (dict(), []))
@@ -37,7 +37,7 @@ class MyTestCase(unittest.TestCase):
         instructions = []
         for instr_json in instruction_jsons:
             instructions.append(factory.create_instruction_json_format(instr_json))
-        uop_creation = UninterpretedOpcodeTermCreation(instructions)
+        uop_creation = UninterpretedOpcodeTermCreation(instructions, [])
         created_dict, created_functions = uop_creation.opcode_rep_with_int()
 
         expected_dict = {'s(0)': 0, 's(1)': 1, 's(2)': 2, 's(3)': 3, 's(4)': 4}
@@ -71,7 +71,7 @@ class MyTestCase(unittest.TestCase):
         instructions = []
         for instr_json in instruction_jsons:
             instructions.append(factory.create_instruction_json_format(instr_json))
-        uop_creation = UninterpretedOpcodeTermCreation(instructions, Sort.integer)
+        uop_creation = UninterpretedOpcodeTermCreation(instructions, [], Sort.integer)
         created_dict, created_functions = uop_creation.opcode_rep_with_uf()
 
         add_f = Function('add', Sort.integer, Sort.integer, Sort.integer)
@@ -118,7 +118,7 @@ class MyTestCase(unittest.TestCase):
         for instr_json in instruction_jsons:
             instructions.append(factory.create_instruction_json_format(instr_json))
 
-        uop_creation = UninterpretedOpcodeTermCreation(instructions, Sort.uninterpreted)
+        uop_creation = UninterpretedOpcodeTermCreation(instructions, [], Sort.uninterpreted)
         created_dict, created_functions = uop_creation.opcode_rep_with_uf()
 
         push_ini = Function('push', Sort.uninterpreted)
@@ -164,7 +164,7 @@ class MyTestCase(unittest.TestCase):
         instructions = []
         for instr_json in instruction_jsons:
             instructions.append(factory.create_instruction_json_format(instr_json))
-        uop_creation = UninterpretedOpcodeTermCreation(instructions)
+        uop_creation = UninterpretedOpcodeTermCreation(instructions, [])
         created_dict, created_functions = uop_creation.opcode_rep_with_stack_vars()
 
         s_0_f = Function('s_0', Sort.integer)
@@ -176,6 +176,106 @@ class MyTestCase(unittest.TestCase):
         expected_dict = {'s(0)': s_0_f(), 's(1)': s_1_f(), 's(2)': s_2_f(), 's(3)': s_3_f(), 's(4)': s_4_f()}
 
         expected_functions = [s_0_f, s_1_f, s_2_f, s_3_f, s_4_f]
+        self.assertDictEqual(created_dict, expected_dict)
+        self.assertListEqual(created_functions, expected_functions)
+
+    def test_uf_different_sort_initial_stack(self):
+        instruction_jsons = [
+            {"id": 'INSTR_0', 'outpt_sk': ["s(0)"], 'inpt_sk': [], 'gas': 0, 'disasm': "PUSH", 'opcode': "a",
+             'size': 0, 'storage': False, 'commutative': False},
+            {"id": 'INSTR_1', 'outpt_sk': ["s(1)"], 'inpt_sk': [], 'gas': 0, 'disasm': "PUSH [$]", 'opcode': "a",
+             'size': 0, 'storage': False, 'commutative': True},
+            {"id": 'INSTR_2', 'outpt_sk': ["s(2)"], 'inpt_sk': ["s(0)", "s(0)", "s(1)"], 'gas': 0, 'disasm': "PUSH #[$]",
+             'opcode': "a",
+             'size': 0, 'storage': False, 'commutative': False},
+            {"id": 'INSTR_3', 'outpt_sk': ["s(3)"], 'inpt_sk': ["s(2)", "s(0)", "s(2)"], 'gas': 0, 'disasm': "SUB",
+             'opcode': "a",
+             'size': 0, 'storage': False, 'commutative': True},
+            {"id": 'INSTR_4', 'outpt_sk': ["s(4)"], 'inpt_sk': ["s(3)", "s(1)", "s(2)"], 'gas': 0, 'disasm': "ADDMOD",
+             'opcode': "a", 'size': 0, 'storage': False, 'commutative': False},
+            {"id": 'INSTR_5', 'outpt_sk': [], 'inpt_sk': ["s(0)", "s(2)"], 'gas': 0, 'disasm': "a",
+             'opcode': "a", 'size': 0, 'storage': True, 'commutative': False},
+            {"id": 'INSTR_6', 'outpt_sk': [], 'inpt_sk': ["s(2)", "s(3)"], 'gas': 0, 'disasm': "a",
+             'opcode': "a", 'size': 0, 'storage': True, 'commutative': False},
+            {"id": 'INSTR_7', 'outpt_sk': [], 'inpt_sk': ["s(4)", "s(1)"], 'gas': 0, 'disasm': "a",
+             'opcode': "a", 'size': 0, 'storage': True, 'commutative': False},
+        ]
+        factory = InstructionFactory()
+        instructions = []
+        for instr_json in instruction_jsons:
+            instructions.append(factory.create_instruction_json_format(instr_json))
+
+        uop_creation = UninterpretedOpcodeTermCreation(instructions, ["s(5)", "s(6)"], Sort.uninterpreted)
+        created_dict, created_functions = uop_creation.opcode_rep_with_uf()
+
+        push_ini = Function('push', Sort.uninterpreted)
+        push_1_f = Function('push[$]', Sort.uninterpreted)
+        push_2_f = Function('push#[$]', Sort.uninterpreted, Sort.uninterpreted, Sort.uninterpreted, Sort.uninterpreted)
+        sub_f = Function('sub', Sort.uninterpreted, Sort.uninterpreted, Sort.uninterpreted, Sort.uninterpreted)
+        addmod_f = Function('addmod', Sort.uninterpreted, Sort.uninterpreted, Sort.uninterpreted, Sort.uninterpreted)
+        s_5 = Function('s_0', Sort.uninterpreted)
+        s_6 = Function('s_1', Sort.uninterpreted)
+
+        instr_0 = push_ini()
+        instr_1 = push_1_f()
+        instr_2 = push_2_f(instr_0, instr_0, instr_1)
+        instr_3 = sub_f(instr_2, instr_0, instr_2)
+        instr_4 = addmod_f(instr_3, instr_1, instr_2)
+        instr_5 = s_5()
+        instr_6 = s_6()
+
+        expected_dict = {'s(0)': instr_0, 's(1)': instr_1, 's(2)': instr_2, 's(3)': instr_3,
+                         's(4)': instr_4, 's(5)': instr_5, 's(6)': instr_6}
+
+        expected_functions = [s_5, s_6, push_ini, push_1_f, push_2_f, sub_f, addmod_f]
+        self.assertDictEqual(created_dict, expected_dict)
+        self.assertListEqual(created_functions, expected_functions)
+
+    def test_stack_vars_initial_stack(self):
+        instruction_jsons = [
+            {"id": 'INSTR_0', 'outpt_sk': ["s(0)"], 'inpt_sk': [5, 6], 'gas': 0, 'disasm': "ADD", 'opcode': "a",
+             'size': 0, 'storage': False, 'commutative': False},
+            {"id": 'INSTR_1', 'outpt_sk': ["s(1)"], 'inpt_sk': [], 'gas': 0, 'disasm': "PUSH [$]", 'opcode': "a",
+             'size': 0, 'storage': False, 'commutative': True},
+            {"id": 'INSTR_2', 'outpt_sk': ["s(2)"], 'inpt_sk': ["s(0)", 7, "s(1)"], 'gas': 0, 'disasm': "PUSH #[$]",
+             'opcode': "a",
+             'size': 0, 'storage': False, 'commutative': False},
+            {"id": 'INSTR_3', 'outpt_sk': ["s(3)"], 'inpt_sk': ["s(2)", 8, "s(2)"], 'gas': 0, 'disasm': "SUB",
+             'opcode': "a",
+             'size': 0, 'storage': False, 'commutative': True},
+            {"id": 'INSTR_4', 'outpt_sk': ["s(4)"], 'inpt_sk': ["s(3)", "s(1)", "s(2)"], 'gas': 0,
+             'disasm': "ADDMOD",
+             'opcode': "a", 'size': 0, 'storage': False, 'commutative': False},
+            {"id": 'INSTR_5', 'outpt_sk': [], 'inpt_sk': ["s(0)", "s(2)"], 'gas': 0, 'disasm': "a",
+             'opcode': "a", 'size': 0, 'storage': True, 'commutative': False},
+            {"id": 'INSTR_6', 'outpt_sk': [], 'inpt_sk': ["s(2)", "s(3)"], 'gas': 0, 'disasm': "a",
+             'opcode': "a", 'size': 0, 'storage': True, 'commutative': False},
+            {"id": 'INSTR_7', 'outpt_sk': [], 'inpt_sk': ["s(4)", "s(1)"], 'gas': 0, 'disasm': "a",
+             'opcode': "a", 'size': 0, 'storage': True, 'commutative': False},
+        ]
+        factory = InstructionFactory()
+        instructions = []
+        for instr_json in instruction_jsons:
+            instructions.append(factory.create_instruction_json_format(instr_json))
+        uop_creation = UninterpretedOpcodeTermCreation(instructions, ["s(8)", "s(10)"])
+        created_dict, created_functions = uop_creation.opcode_rep_with_stack_vars()
+
+        # Functions in initial stack
+        s_8_f = Function('s_0', Sort.integer)
+        s_10_f = Function('s_1', Sort.integer)
+
+        # Functions from uninterpreted operations
+        s_0_f = Function('s_2', Sort.integer)
+        s_1_f = Function('s_3', Sort.integer)
+        s_2_f = Function('s_4', Sort.integer)
+        s_3_f = Function('s_5', Sort.integer)
+        s_4_f = Function('s_6', Sort.integer)
+
+        expected_dict = {'s(0)': s_0_f(), 's(1)': s_1_f(), 's(2)': s_2_f(), 's(3)': s_3_f(), 's(4)': s_4_f(),
+                         's(8)': s_8_f(), 's(10)': s_10_f()}
+
+        expected_functions = [s_8_f, s_10_f, s_0_f, s_1_f, s_2_f, s_3_f, s_4_f]
+
         self.assertDictEqual(created_dict, expected_dict)
         self.assertListEqual(created_functions, expected_functions)
 
