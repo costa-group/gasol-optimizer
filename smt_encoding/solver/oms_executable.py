abd990c1fd4cb4ce88fdf1e5fa2407e3c9597423
alexcere <48130030+alexcere@users.noreply.github.com>
2022-06-11 14:03:32 +0200

    #18 OMS executable added

---
+++ smt_encoding/solver/oms_executable.py
@@ -0,0 +1,26 @@
+from smt_encoding.solver.solver_from_executable import SolverFromExecutable, AssertSoft, List, translate_formula
+from global_params.paths import oms_exec
+
+
+class OMSExecutable(SolverFromExecutable):
+
+    def __init__(self, file_path: str):
+        super(OMSExecutable, self).__init__(oms_exec, file_path)
+        # Need this option to produce models
+        self.set_option("produce-models", "true")
+
+    def write_soft(self, soft_constraint: AssertSoft) -> str:
+        if soft_constraint.group is None:
+            return f"(assert-soft {translate_formula(soft_constraint.formula)} :weight {str(soft_constraint.weight)})"
+        else:
+            return f"(assert-soft {translate_formula(soft_constraint.formula)} :weight {str(soft_constraint.weight)} " \
+                   f":id {soft_constraint.group})"
+
+    def load_model(self) -> List[str]:
+        return ["(get-objectives)", "(get-model)"]
+
+    def cost_function(self) -> str:
+        return "(minimize cost)"
+
+    def command_line(self) -> str:
+        return f"{oms_exec} {self._file_path} -optimization=True"
