3b6df7f82a1c123cd9e7a989ac3a49b46100b933
alexcere <48130030+alexcere@users.noreply.github.com>
2022-10-26 17:06:23 +0200

    Fix wrong order between SHL and PUSHLIB

--- sfs_generator/gasol_optimization.py
+++ sfs_generator/gasol_optimization.py
@@ -1031,6 +1031,16 @@ def get_involved_vars(instr,var):
 
         funct = "shr"
 
+    # pushlib must come before shl to avoid overlapping
+    elif instr.startswith("pushlib("):
+        instr_new = instr.strip("\n")
+        pos = instr_new.find("pushlib(")
+        arg0 = instr[pos+8:-1]
+        var0 = arg0.strip()
+        var_list.append(var0)
+
+        funct = "pushlib"
+
     elif instr.startswith("shl("):
         instr_new = instr.strip("\n")
         pos = instr_new.find("shl(")
@@ -1270,16 +1280,6 @@ def get_involved_vars(instr,var):
         var_list.append(var0)
 
         funct = "pushtag"
-
-    elif instr.startswith("pushlib("):
-        instr_new = instr.strip("\n")
-        pos = instr_new.find("pushlib(")
-        arg0 = instr[pos+8:-1]
-        var0 = arg0.strip()
-        var_list.append(var0)
-
-        funct = "pushlib"
-
 
     elif instr.startswith("push#[$]("):
         instr_new = instr.strip("\n")
@@ -2302,6 +2302,10 @@ def generate_userdefname(u_var,funct,args,arity,init=False):
     elif funct.find("shr") !=-1:
         instr_name = "SHR"
 
+    # pushlib must come before shl to avoid overlapping
+    elif funct.find("pushlib")!=-1:
+        instr_name = "PUSHLIB"
+
     elif funct.find("shl") !=-1:
         instr_name = "SHL"
 
@@ -2446,9 +2450,6 @@ def generate_userdefname(u_var,funct,args,arity,init=False):
 
     elif funct.find("pushimmutable")!=-1:
         instr_name = "PUSHIMMUTABLE"
-
-    elif funct.find("pushlib")!=-1:
-        instr_name = "PUSHLIB"
 
     #TODO: Add more opcodes
     
