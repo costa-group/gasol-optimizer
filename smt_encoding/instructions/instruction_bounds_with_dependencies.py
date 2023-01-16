bbaec67ebd7fc5dc3532a5be7016b32209178fcb
alexcere <48130030+alexcere@users.noreply.github.com>
2023-01-16 20:13:20 +0100

    Improve ub for instruction performance using toposort

--- smt_encoding/instructions/instruction_bounds_with_dependencies.py
+++ smt_encoding/instructions/instruction_bounds_with_dependencies.py
@@ -1,3 +1,4 @@
+import copy
 from collections import defaultdict
 from smt_encoding.instructions.encoding_instruction import ThetaValue, InstructionSubset, Id_T
 from smt_encoding.instructions.uninterpreted_instruction import UninterpretedInstruction
@@ -144,89 +145,122 @@ def generate_lower_bound_from_instr_alternative(dependency_graph : Dict[Id_T, Li
     return number_of_instructions_to_execute
 
 
+def update_current_index(instr_id: Id_T, bounds_positions: Dict[Id_T, List[int]], new_idx: int) -> None:
+    # First value associated corresponds to the min so far, and the other to the max
+    if instr_id not in bounds_positions:
+        bounds_positions[instr_id] = [new_idx, new_idx]
+    else:
+        current_min, current_max = bounds_positions[instr_id]
+        bounds_positions[instr_id] = [min(new_idx, current_min), max(new_idx, current_max)]
+
+
 # First time an instruction cannot appear is b0-h, where h is the tree height. We recursively obtain this value,
 # taking into account that we may have different trees and the final value is the min for all possible ones
-def update_with_tree_level(instr : UninterpretedInstruction, current_idx : int, stack_elem_to_id : Dict[str, Id_T],
-                           instr_by_id_dict : Dict[Id_T, UninterpretedInstruction], dependency_graph: Dict[Id_T, List[Id_T]],
-                           first_position_instr_cannot_appear : Dict[Id_T, int]) -> None:
+def update_with_tree_level(stack_elem_to_id : Dict[str, Id_T], instr_by_id_dict : Dict[Id_T, UninterpretedInstruction],
+                           dependency_graph: Dict[Id_T, List[Id_T]], topological_order: List[Id_T], bounds_positions: Dict[Id_T, List[int]]) -> None:
 
-    instr_id = instr.id
-
-    # If the instruction can only appear once, then we need to consider the min among all positions in which it cannot
-    # appear. Otherwise, consider the max, because it could appear repeated in several of those positions
-    if instr_id in first_position_instr_cannot_appear and (
-            (instr.unique_ui and current_idx >= first_position_instr_cannot_appear[instr_id]) or (
-            not instr.unique_ui and current_idx <= first_position_instr_cannot_appear[instr_id])):
-        return
-
-    first_position_instr_cannot_appear[instr_id] = current_idx
-
-    # If an instruction is not commutative, then only topmost previous element can be obtained
-    # just before and the remaining ones need at least a SWAP to be placed in their position
-    dependent_instr_ids = dependency_graph[instr_id]
-
-    # Already analyzed instruction ids
-    analyzed_instr_ids = set()
-    previous_idx = current_idx - 1
-    for prev_instr_stack_var in instr.input_stack:
-
-        if prev_instr_stack_var in stack_elem_to_id:
-            prev_instr_id = stack_elem_to_id[prev_instr_stack_var]
-            analyzed_instr_ids.add(prev_instr_id)
-            update_with_tree_level(instr_by_id_dict[prev_instr_id], previous_idx, stack_elem_to_id, instr_by_id_dict,
-                                   dependency_graph, first_position_instr_cannot_appear)
+    for instr_id in topological_order:
+        instr = instr_by_id_dict[instr_id]
 
         # If an instruction is not commutative, then only topmost previous element can be obtained
         # just before and the remaining ones need at least a SWAP to be placed in their position
-        if instr.instruction_subset != InstructionSubset.comm:
-            previous_idx = current_idx - 2
+        dependent_instr_ids = dependency_graph[instr_id]
 
-    # There can be other related instructions that are not considered before (mem instructions). As we do not know exactly
-    # where they can appear, we assume worst case (current_idx - 1)
-    for prev_instr_id in set(dependent_instr_ids).difference(analyzed_instr_ids):
+        # Already analyzed instruction ids
+        analyzed_instr_ids = set()
+        min_index, max_index = bounds_positions[instr_id]
+        index_l = [min_index - 1, max_index - 1]
 
-        # Special case: PUSH basic opcode is just omitted
-        if prev_instr_id == "PUSH":
-            continue
-        update_with_tree_level(instr_by_id_dict[prev_instr_id], current_idx - 1, stack_elem_to_id, instr_by_id_dict,
-                               dependency_graph, first_position_instr_cannot_appear)
+        for prev_instr_stack_var in instr.input_stack:
+            for possible_index in index_l:
+
+                if prev_instr_stack_var in stack_elem_to_id:
+                    prev_instr_id = stack_elem_to_id[prev_instr_stack_var]
+                    analyzed_instr_ids.add(prev_instr_id)
+                    update_current_index(prev_instr_id, bounds_positions, possible_index)
+
+            # If an instruction is not commutative, then only topmost previous element can be obtained
+            # just before and the remaining ones need at least a SWAP to be placed in their position
+            if instr.instruction_subset != InstructionSubset.comm:
+                index_l = [min_index - 2, max_index - 2]
+
+        # There can be other related instructions that are not considered before (mem instructions). As we do not know
+        # exactly where they can appear, we assume worst case (current_idx - 1)
+        index_l = [min_index - 1, max_index - 1]
+
+        for prev_instr_id in set(dependent_instr_ids).difference(analyzed_instr_ids):
+            for possible_index in index_l:
+                update_current_index(prev_instr_id, bounds_positions, possible_index)
+
+def initialize_bound_positions_for_ub(b0: int, final_stack_ids: List[Id_T], maximal_mem_ids: List[Id_T],
+                                      bounds_positions: Dict[Id_T, List[int]]):
+    # If first instruction corresponds to top of the stack, we initialize the search with b0. Otherwise,
+    # it means that another extra instruction must be performed after this, and thus, we start searching with b0 - 1.
+    b0_aux = b0
+
+    # We consider instructions in the final stack, as they determine which position is the last possible one (following
+    # the dependencies to reach it). We are assuming each other instruction is related to these instructions. Otherwise,
+    # it would mean that there exists some intermediate instructions that do not affect the final results and thus,
+    # they must be omitted.
+    for final_instr_id in final_stack_ids:
+
+        # Only those stack elements tied to an instruction must be considered, not initial stack values
+        if final_instr_id is not None:
+            update_current_index(final_instr_id, bounds_positions, b0_aux)
+
+        # If it isn't top of the stack, another instruction must go before it (SWAP or DUP). Only works once
+        b0_aux = b0 - 1
+
+    for mem_instr_id in maximal_mem_ids:
+        update_current_index(mem_instr_id, bounds_positions, b0)
+
+
+def iterative_topological_sort(dependency_graph : Dict[Id_T, List[Id_T]], start:Id_T):
+    seen = set()
+    stack = []    # path variable is gone, stack and order are new
+    order = []    # order will be in reverse order at first
+    q = [start]
+    while q:
+        v = q.pop()
+        if v not in seen:
+            seen.add(v)
+            q.extend(dependency_graph[v])
+
+            while stack and v not in dependency_graph[stack[-1]]:
+                order.append(stack.pop())
+            stack.append(v)
+
+    return stack + order[::-1]   # new return value!
 
 
 # Generates a dict that given b0, returns the first position in which a instruction cannot appear
 # due to dependencies with other instructions.
 def generate_first_position_instr_cannot_appear(b0 : int, stack_elem_to_id : Dict[str, Id_T],
                                                 final_stack : List[str], dependency_graph : Dict[Id_T, List[Id_T]],
-                                                mem_ids : List[Id_T], instr_by_id_dict : Dict[Id_T, UninterpretedInstruction]) -> Dict[Id_T, int]:
-    if 'PUSH' in dependency_graph:
-        first_position_instr_cannot_appear = {'PUSH' : b0}
-    else:
-        first_position_instr_cannot_appear = {}
+                                                maximal_mem_ids : List[Id_T],
+                                                instr_by_id_dict : Dict[Id_T, UninterpretedInstruction]) -> Dict[Id_T, int]:
+    # Assume there is always at least one element
+    if dependency_graph == []:
+        return dict()
 
-    # We consider instructions in the final stack, as they determine which position is the last possible one (following
-    # the dependencies to reach it). We are assuming each other instruction is related to these instructions. Otherwise,
-    # it would mean that there exists some intermediate instructions that do not affect the final results and thus,
-    # they must be omitted.
-    final_stack_with_ids = [stack_elem_to_id[stack_var] if stack_var in stack_elem_to_id else None for stack_var in final_stack]
+    bound_positions = dict()
 
-    # If first instruction corresponds to top of the stack, we initialize the search with b0. Otherwise,
-    # it means that another extra instruction must be performed after this, and thus, we start searching with b0 - 1.
-    b0_aux = b0
+    final_stack_ids = [stack_elem_to_id[stack_var] if stack_var in stack_elem_to_id else None for stack_var in final_stack]
+    initialize_bound_positions_for_ub(b0, final_stack_ids, maximal_mem_ids, bound_positions)
 
-    for final_instr_id in final_stack_with_ids:
+    maximal_elements = list(set(instr_id for instr_id in dependency_graph).difference(
+        set(instr_id for id_list in dependency_graph.values() for instr_id in id_list)))
+    extended_dependency_graph = copy.deepcopy(dependency_graph)
 
-        # Only those stack elements tied to an instruction must be considered, not initial stack values
-        if final_instr_id is not None:
-            update_with_tree_level(instr_by_id_dict[final_instr_id], b0_aux, stack_elem_to_id, instr_by_id_dict,
-                               dependency_graph, first_position_instr_cannot_appear)
+    # We add a "dummy element" to function as the initial value
+    extended_dependency_graph['dummy'] = maximal_elements
+    topo_order = iterative_topological_sort(extended_dependency_graph, 'dummy')
+    topo_order.pop(0)
 
-        # If it isn't top of the stack, another instruction must go before it (SWAP or DUP). Only works once
-        b0_aux = b0 - 1
+    update_with_tree_level(stack_elem_to_id, instr_by_id_dict, dependency_graph, topo_order, bound_positions)
 
-    for final_instr_id in mem_ids:
-        update_with_tree_level(instr_by_id_dict[final_instr_id], b0, stack_elem_to_id, instr_by_id_dict,
-                               dependency_graph, first_position_instr_cannot_appear)
-
-    return first_position_instr_cannot_appear
+    return {instr_id: (bounds[0] if instr_by_id_dict[instr_id].unique_ui else bounds[1])
+            for instr_id, bounds in bound_positions.items()}
 
 
 class InstructionBoundsWithDependencies(InstructionBounds):
@@ -241,7 +275,11 @@ class InstructionBoundsWithDependencies(InstructionBounds):
 
         dependency_graph : Dict[Id_T, List[Id_T]] = generate_dependency_graph_minimum(instructions, order_tuples, stack_element_to_id_dict)
 
-        mem_ids : List[Id_T] = [instruction.id for instruction in instructions if instruction.instruction_subset == InstructionSubset.store]
+        # To obtain the upper bounds, we need to consider the maximal STORE's i.e. STORE instructions that don't have
+        # to appear before another memory instruction
+        dependendent_mem_ids = set(tup[0] for tup in order_tuples)
+        non_dependent_mem_ids : List[Id_T] = list(set(instruction.id for instruction in instructions
+                                                      if instruction.instruction_subset == InstructionSubset.store).difference(dependendent_mem_ids))
 
         self._b0 = b0
         self._initial_idx = initial_idx
@@ -250,7 +288,7 @@ class InstructionBoundsWithDependencies(InstructionBounds):
         self._lower_bound_by_theta_value = {instr_by_id_dict[instr_id].theta_value: lb
                                             for instr_id, lb in lower_bound_by_id.items() if instr_id != 'PUSH'}
         first_position_not_instr_by_id = generate_first_position_instr_cannot_appear(b0, stack_element_to_id_dict, final_stack,
-                                                                                     dependency_graph, mem_ids, instr_by_id_dict)
+                                                                                     dependency_graph, non_dependent_mem_ids, instr_by_id_dict)
         self._first_position_not_instr_by_theta_value =  {instr_by_id_dict[instr_id].theta_value : lb
                                                           for instr_id, lb in first_position_not_instr_by_id.items()
                                                           if instr_id != 'PUSH'}
