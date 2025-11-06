37827b886764dce6721fc1028969b32148a8a352
tutugordillo <tutugordillo@gmail.com>
2025-11-06 01:54:53 +0100

    Close #50

--- sfs_generator/ir_block.py
+++ sfs_generator/ir_block.py
@@ -495,9 +495,9 @@ def translateOpcodes30(opcode, value, index_variables):
         instr = "extcodecopy("+v0+","+v1+","+v2+","+v3+")"
 
     elif opcode == "EXTCODEHASH":
-        _, updated_variables = get_consume_variable(index_variables)
+        v0, updated_variables = get_consume_variable(index_variables)
         v1, updated_variables = get_new_variable(updated_variables)
-        instr = v1+" = extcodehash("+v1+")"  
+        instr = v1+" = extcodehash("+v0+")"  
 
 
     elif opcode == "RETURNDATASIZE":
